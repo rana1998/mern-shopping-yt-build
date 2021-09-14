d6bad752313d1df490edd98c94bb8981ceb939fa
rana1998 <2015Kshitij14@gmail.com>
2021-09-15 00:20:55 +0530

    screend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,20 @@
 import './HomeScreen.css';
+import Product from '../components/Product';
 
 const HomeScreen = () => {
     return (
         <div className="homescreen">
-            Home Screen
+            <h2 className="homescreen__title">Latest product</h2>
+            <div className="homescreen__products">
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+
+            </div>
         </div>
     )
 }
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -3,7 +3,43 @@ import './ProductScreen.css'
 const ProductScreen = () => {
     return (
         <div className="productscreen"> 
-            Product Screen
+            <div className="productscreen__left">
+                <div className="left_image">
+                    <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt="product name"/>
+                </div>
+
+                <div className="left__info">
+                    <p className="left__name"> Product 1</p>
+                    <p> Price : $499.9</p>
+                    <p> Description :                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+</p>
+                </div>
+
+            </div>
+            <div className="productscreen__right">
+                <div className="right__info">
+                    <p>
+                        Price:<span>$499.99</span>
+                    </p>
+                    <p>
+                        Status: <span>  In stock</span>
+                    </p>
+
+                    <p>
+                        Qty
+                        <select>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                        </select>
+                    </p>
+
+                    <p>
+                        <button tyoe="button">Add to cart</button>
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
