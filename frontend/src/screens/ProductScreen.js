8532dc8f2f7ffa7c4822b890dc0d763e770af7f8
rana1998 <2015Kshitij14@gmail.com>
2021-09-15 22:49:17 +0530

    cartscreen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -4,15 +4,15 @@ const ProductScreen = () => {
     return (
         <div className="productscreen"> 
             <div className="productscreen__left">
-                <div className="left_image">
+                <div className="left__image">
                     <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt="product name"/>
                 </div>
 
                 <div className="left__info">
                     <p className="left__name"> Product 1</p>
                     <p> Price : $499.9</p>
-                    <p> Description :                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-</p>
+                    <p> Description :Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    </p>
                 </div>
 
             </div>
